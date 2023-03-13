b55ec0363aadbf077b6556afb619a91ef8feed3e
IsaacAmorteg <isaacamortegc@outlook.com>
2023-03-13 15:33:33 -0500

    Initialization of the prproject

---
+++ TrianglesApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrianglesApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter coordinate x of dot A: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+        }
+
+    }
+}
